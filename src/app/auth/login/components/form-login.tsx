95ec28918cc46d6aaffd4e5113b547c75163c469
Gabriel Girami <rm98017@fiap.com.br>
2023-11-24 21:55:22 -0300

    modified responsive header

--- src/app/auth/login/components/form-login.tsx
+++ src/app/auth/login/components/form-login.tsx
@@ -3,7 +3,7 @@
 import { useState } from 'react'
 import { signIn } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
-import style from 'C:/Users/Dell/Documents/GS-WEB/src/style/form-login.module.scss'
+import style from '@/style/form-login.module.scss'
 
 const LoginForm = () => {
     const router = useRouter()
