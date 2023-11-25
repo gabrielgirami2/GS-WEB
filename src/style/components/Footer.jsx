26ed02e6b8316571c093e5358b94f45189bb5f07
Felipe Bernardes <128436489+Bernas01@users.noreply.github.com>
2023-11-24 22:50:08 -0300

    Update Footer.jsx

--- src/style/components/Footer.jsx
+++ src/style/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
           <p>Work Team: </p>
           <p>Gabriel Girami | RM: 98017 | 1TDSPZ | Responsive Web Development</p>
           <p>Gustavo Vinhola | RM: 98826 | 1TDSPZ | AI & Chatbot</p>
-          <p>Felipe Bernardes | RM:  | 1TDSPZ | Computational Thinking Using Python</p>
+          <p>Felipe Bernardes | RM: 98866 | 1TDSPZ | Computational Thinking Using Python</p>
           <p>Felipe Pinheiro | RM: 550244 | 1TDSPZ | Java and Software Design E Total Experience</p>
           <p>Victor Fanfoni | RM: 99173  | 1TDSPZ | Building Relational Database</p>
       </div>
